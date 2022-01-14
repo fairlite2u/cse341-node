b140e6f9883529140df60233a8587334fc32e060
fairlite2u <pyl95001@byui.edu>
2022-01-13 21:41:37 -0700

    wk 2 done: 404 fixed, price & desc inputs added

--- app.js
+++ app.js
@@ -1,35 +1,61 @@
+// const path = require('path');
+
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// // const expressHbs = require('express-handlebars'); // To import Handlebars
+
+// const app = express();
+
+// app.set('view engine', 'ejs');
+// app.set('views', 'views');
+
+// // // To set view engine to Handlebars
+// // app.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'}));
+// // app.set('view engine', 'hbs');
+// // app.set('views', 'views');
+
+// // // To set view engine to Pug
+// // app.set('view engine', 'pug');
+// // app.set('views', 'views');
+
+// const adminData = require('./routes/admin');
+// const shopRoutes = require('./routes/shop');
+
+// app.use(bodyParser.urlencoded({extended: false}));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/admin', adminData.routes);
+// app.use(shopRoutes);
+
+// app.use((req, res, next) => {
+//   // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+//   res.status(404).render('404', {pageTitle: 'Page Not Found'});
+// });
+
+// app.listen(3000);
+
 const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars'); // To import Handlebars
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-// // To set view engine to Handlebars
-// app.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'}));
-// app.set('view engine', 'hbs');
-// app.set('views', 'views');
-
-// // To set view engine to Pug
-// app.set('view engine', 'pug');
-// app.set('views', 'views');
-
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-  res.status(404).render('404', {pageTitle: 'Page Not Found'});
+  res.status(404).render('404', { pageTitle: 'Page Not Found', path: '',  });
 });
 
 app.listen(3000);
+
--- routes/admin.js
+++ routes/admin.js
@@ -23,7 +23,7 @@ router.get('/add-product', (req, res, next) => {
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
   // console.log(req.body);
-  products.push({ title: req.body.title });
+  products.push({ title: req.body.title, price: req.body.price, description: req.body.description });
   res.redirect('/');
 });
 
