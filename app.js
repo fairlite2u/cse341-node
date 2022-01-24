c1656db00df733980a1a74d3feab4b1e8c413408
fairlite2u <pyl95001@byui.edu>
2022-01-24 08:44:46 -0700

    trying again

--- app.js
+++ app.js
@@ -1,40 +1,5 @@
-// const path = require('path');
-
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// // const expressHbs = require('express-handlebars'); // To import Handlebars
-
-// const app = express();
-
-// app.set('view engine', 'ejs');
-// app.set('views', 'views');
-
-// // // To set view engine to Handlebars
-// // app.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'}));
-// // app.set('view engine', 'hbs');
-// // app.set('views', 'views');
-
-// // // To set view engine to Pug
-// // app.set('view engine', 'pug');
-// // app.set('views', 'views');
-
-// const adminData = require('./routes/admin');
-// const shopRoutes = require('./routes/shop');
-
-// app.use(bodyParser.urlencoded({extended: false}));
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use('/admin', adminData.routes);
-// app.use(shopRoutes);
-
-// app.use((req, res, next) => {
-//   // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-//   res.status(404).render('404', {pageTitle: 'Page Not Found'});
-// });
-
-// app.listen(3000);
-
 const path = require('path');
+const PATH = process.env.PORT || 5000;
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -57,5 +22,5 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+app.listen(PORT);
 
