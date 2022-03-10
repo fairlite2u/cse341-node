8c859751ca624c1b70e30a43ad7b1a9e3c487654
fairlite2u <pyl95001@byui.edu>
2022-03-10 11:39:56 -0700

    adding payments

--- routes/shop.js
+++ routes/shop.js
@@ -4,6 +4,7 @@ const express = require('express');
 
 const shopController = require('../controllers/shop');
 const isAuth = require('../middleware/is-auth');
+const { route } = require('./admin');
 
 const router = express.Router();
 
@@ -19,7 +20,13 @@ router.post('/cart', isAuth, shopController.postCart);
 
 router.post('/cart-delete-item', isAuth, shopController.postCartDeleteProduct);
 
-router.post('/create-order', isAuth, shopController.postOrder);
+router.get('/checkout', isAuth, shopController.getCheckout); // for payments
+
+router.get('/checkout/success', shopController.getCheckoutSuccess); // for payments
+
+router.get('/checkout/cancel', shopController.getCheckout); // for payments
+
+// router.post('/create-order', isAuth, shopController.postOrder); // route not needed when using Stripe
 
 router.get('/orders', isAuth, shopController.getOrders);
 
